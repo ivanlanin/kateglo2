aa4477571b067ba39cdfb7a4ce0327280f5573eb
Ivan Lanin <ivan.lanin@gmail.com>
2026-02-14 11:14:39 +0700

    Menerapkan lint

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -7,7 +7,7 @@ const logger = require('../config/logger');
 /**
  * 404 Not Found handler
  */
-const notFoundHandler = (req, res, next) => {
+const notFoundHandler = (req, res, _next) => {
   res.status(404).json({
     error: 'Not Found',
     message: `Route ${req.method} ${req.path} not found`,
@@ -18,7 +18,7 @@ const notFoundHandler = (req, res, next) => {
 /**
  * Global error handler
  */
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, _next) => {
   logger.error(err.message, { stack: err.stack });
 
   // Default error
